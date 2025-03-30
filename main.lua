edc77548431fc84790183d985541276e32098bdd
phortuin <phortuin@users.noreply.github.com>
2025-03-30 20:17:22 +0200

    Add gameplay ideas

--- main.lua
+++ main.lua
@@ -2,15 +2,21 @@
 require "lib.init"
 local Gameplay = require "src.states.gameplay"
 
--- als je te laat een monster pakt, pakt hij geld van je af, of health
--- een monster moet je 2x klikken
--- een zwaardje zorgt ervoor dat je een monster maar 1x hoeft te klikken, blijft 5 kliks geldig
--- hover over mob verandert cursor
--- paddestoel die te lang blijft, wordt giftig
--- giftige paddestoel spawnt meer paddestoelen (gpt zei "en monsters" :D) en als je daar overheen muist gaat je muis trager. of verlies je health?
--- effect/event triggers en handlers
--- health bars!
--- levels, dus na bijv 20 mobs ga je een level omhoog en wordt het moeilijker
+--[x] als je te laat een monster pakt, pakt hij geld van je af, of health
+--[ ] een zwaardje maakt je hits sterker, blijft 5 kliks geldig
+--[ ] hover over mob verandert cursor
+--[x] paddestoel die te lang blijft, wordt giftig
+--[ ] giftige paddestoel spawnt meer paddestoelen (gpt zei "en monsters" :D) en als je daar overheen muist gaat je muis trager. of verlies je health?
+--[ ] effect/event triggers en handlers
+--[x] health bars!
+--[ ] als boss een monster raakt, healt hij. of het andere monster wordt ook een boss
+--[ ] als boss te lang blijft, wordt hij een giftige boss (of een boss die monsters spawnt)
+--[x] powerup die alle monsters doodt
+--[ ] levels, na verloop van tijd, x mobs of x punten wordt het moeilijker: minimaal 2 mobs in het scherm, snellere aanvalstijd, boss is sterker
+--[x] bolt maakt goede shroom rot, of haalt rotte shroom weg
+--[x] coins moeten naar je pijl toe trekken
+--[ ] monsters die je niet aan mag raken (ze houden je pijl vast)
+--[ ] monsters in het donker; hoe verder af van het midden, hoe donkerder
 
 local currentState
 
